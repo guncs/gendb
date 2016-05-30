583dfedc2352e4640c6669d4d6ef028bd8719cf4
Gunce Yilmaz <gunceyilmaz@hotmail.com>
2016-05-30 15:41:25 -0500

    working on fputs schema stuff

--- fopen.c
+++ fopen.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
 	//argv[1] : a txt or csv file
-	int x = 0;
 	char buf[80];
+	int r;
+	double d;
+	char str[100];
+	char dest[200];
+	char str1[25];
+	char str2[25];
+
 	printf("Opening1 %s\n", argv[1]);
-	FILE* fileptr = fopen(argv[1], "r");
+	FILE* fileptr = fopen(argv[1], "a+");
+	
+	/*for(int i =0; i < 11; i++){
+		r = rand() % 10000;
+		d = (double)((rand() % 10000) / 7);
+		for(int i = 0; i < 20; i++){
+   			int x = rand() % 25; 
+   			str[i] = (x + 97);
+		} 
+		sprintf(str1, "%d", r);
+		sprintf(str2, "%f", d);
+	   	strcpy(dest, str1);
+	   	strcat(dest, " ");
+	    strcat(dest, str);
+	    strcat(dest, " ");
+	    strcat(dest, str2);
+    	const char *string = dest;
+    	printf("what is string?: %s\n", string); 
+		fputs(string, fileptr);
+	}
+
 	do {
 		fgets(buf, 80, fileptr);
 		if (feof(fileptr)){
 			break; 
 		}
-		//printf("%s", buf);
-	} while(1);
+	} while(1);*/
 
-	fputs("a, b, c, d, e, f, g", fileptr);
-	
 	fclose(fileptr);
 	return 0; 
 }
+
--- fopenwrap.c
+++ fopenwrap.c
@@ -3,14 +3,16 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <libpq-fe.h>
+#include <string.h>
 
 char *conninfo;
 PGconn *conn;
 PGresult *res;
 PGresult *wres;
 int nFields;
-int j = 0;
+int cnt_fget = 0;
 int k = 0;
+int cnt_fput = 0;
 
 static void
 exit_nicely(PGconn *conn)
@@ -35,7 +37,24 @@ FILE* fopen(const char* path, const char* mode) {
         exit_nicely(conn);
     }
     printf("Opening4 %s\n", path);
-    
+
+    res = PQexec(conn, 
+        "CREATE TABLE table3 (id serial primary key); CREATE INDEX ind3 ON table3 (id);");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
+        fprintf(stderr, "CREATE TABLE failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+    printf("Opening5 table created!! \n");
+
+
+	FILE* (*real_fopen)(const char*, const char*) = dlsym(RTLD_NEXT, "fopen");
+    return real_fopen(path, mode);
+}
+
+char* fgets(char *str, int n, FILE *f){
 
     // Start a transaction block
     res = PQexec(conn, "BEGIN");
@@ -45,19 +64,17 @@ FILE* fopen(const char* path, const char* mode) {
     }  
     // Clear memory
     PQclear(res);
-    printf("Opening5 %s\n", path);
-
+    //printf("Opening5 %s\n", f);
 
     //Declare cursor
-    res = PQexec(conn, "DECLARE mydata CURSOR FOR select * from pg_am");
+    res = PQexec(conn, "DECLARE mydata CURSOR FOR select * from tgunc");
     if (PQresultStatus(res) != PGRES_COMMAND_OK){
         fprintf(stderr, "DECLARE CURSOR failed: %s", PQerrorMessage(conn));
         exit_nicely(conn);
     }
     PQclear(res);
-    printf("Opening6 %s\n", path);
-
-
+    //printf("Opening6 %s\n", f);
+    
     //Fetch all the rows
     res = PQexec(conn, "FETCH ALL in mydata");
     if (PQresultStatus(res) != PGRES_TUPLES_OK){
@@ -65,31 +82,18 @@ FILE* fopen(const char* path, const char* mode) {
         exit_nicely(conn);
     }
     nFields = PQnfields(res);
-    printf("Opening7 %s\n", path);
-
-
-    FILE* (*real_fopen)(const char*, const char*) = dlsym(RTLD_NEXT, "fopen");
-    return real_fopen(path, mode);
-}
-
-char* fgets(char *str, int n, FILE *f){
-
-    /*res = PQexec(conn, "FETCH NEXT FROM mydata");
-    if (PQresultStatus(res) != PGRES_TUPLES_OK){
-        fprintf(stderr, "FETCH NEXT failed: %s", PQerrorMessage(conn));
-        exit_nicely(conn);
-    }*/
 
     // print attribute names
-    if(j == 0){
+    if(cnt_fget == 0){
         for (int i = 0; i < nFields; i++){
             printf("%-15s", PQfname(res, i));
         }
         printf("\n\n");
     }
-    j++;
+    cnt_fget++;
 
     // print rows
+    //for (i = firstrow; i < lastrow+1; i++){
     if(k != PQntuples(res)){
         for (int i = 0; i < nFields; i++){
             printf("%-15s", PQgetvalue(res, k, i));
@@ -98,33 +102,32 @@ char* fgets(char *str, int n, FILE *f){
         k++;
     }
 
-
     char* (*real_fgets)(char*, int, FILE*) = dlsym(RTLD_NEXT, "fgets");
     return real_fgets(str, n, f);
 }
 
 int fputs(const char *str, FILE *f){
     
-    char dest[200];
-    strcpy(dest, "INSERT INTO tgunc VALUES (");
-    strcat(dest, str);
-    strcat(dest, ")");
-    const char *string = dest;
+    if(cnt_fput == 0){
+        char dest[200];
+        strcpy(dest, "INSERT INTO tgunc VALUES (");
+        strcat(dest, str);
+        strcat(dest, ")");
+        const char *string = dest;
 
-    printf("Opening 8 : fputs testing:  %s\n\n\n\n", string);
+        printf("Opening 8 : fputs testing:  %s\n\n\n\n", string);
 
-    printf("fputs testing:  %s\n\n\n\n", dest);
+        wres = PQexec(conn, string);
+        if (PQresultStatus(wres) != PGRES_COMMAND_OK)
+        {
+            fprintf(stderr, "INSERT command failed: %s", PQerrorMessage(conn));
+            PQclear(wres);
+            exit_nicely(conn);
+        }
 
-    wres = PQexec(conn, s);
-    if (PQresultStatus(wres) != PGRES_COMMAND_OK)
-    {
-        fprintf(stderr, "INSERT command failed: %s", PQerrorMessage(conn));
-        PQclear(wres);
-        exit_nicely(conn);
+        printf("Opening 9 : fputs testing2\n\n\n\n");
     }
-
-    printf("fputs testing2\n\n\n\n");
-    
+    cnt_fput++;
     int (*real_fputs)(const char*, FILE*) = dlsym(RTLD_NEXT, "fputs");
     return real_fputs(str, f);
 }
@@ -137,14 +140,16 @@ int fclose(FILE *f){
     // close cursor
     res = PQexec(conn, "CLOSE mydata");
     PQclear(res);
-
+    printf("Opening10 \n");
+    
     // end transaction
     res = PQexec(conn, "END");
     PQclear(res);
-
+    //printf("Opening7 %s\n", path);
+    
     // close db connection 
     PQfinish(conn);
-
+    printf("Opening11 \n");
     int (*real_fclose)(FILE*) = dlsym(RTLD_NEXT, "fclose");
     return real_fclose(f);
 }
